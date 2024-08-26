8923617b2f9beb61df56b92d881b449800247806
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-08-25 21:11:46 -0400

    Prototype still not finish

--- script.js
+++ script.js
@@ -19,32 +19,57 @@ function multiply(op1, op2){
     console.log(op1 * op2);
 }
 
-function operate(op1, op2, operator){
+function operate(evaluate){
 
-    switch(operator){
-        case "+":
-            add(op1, op2);
+    switch(evaluate[1]){
+        case " + ":
+            add(evaluate[0], evaluate[2]);
             break;
-        case "-":
-            subtract(op1, op2);
+        case " - ":
+            subtract(evaluate[0], evaluate[2]);
             break;
-        case "*":
-            multiply(op1, op2);
+        case " * ":
+            multiply(evaluate[0], evaluate[2]);
             break;
-        case "/":
-            divide(op1, op2);
+        case " / ":
+            divide(evaluate[0], evaluate[2]);
             break;
     }
 
 }
 
-
-let textField = prompt("Enter text: ").split(" ");
-
-let operand1 = parseInt(textField[0]);
-let operand2 = parseInt(textField[2]);
-let operator = textField[1];
-
-operate(operand1, operand2, operator);
+let buttons = document.querySelectorAll(".button");
+let screen = document.querySelector(".screen");
+let  evaluate = [];
 
 
+buttons.forEach(function(button, index){
+    button.addEventListener("click", function(event){
+
+  
+        
+
+        if(button.textContent === "+"){
+            event.preventDefault();
+        }
+        else{
+            screen.textContent = screen.textContent + button.textContent;
+        }
+        
+    });
+});
+
+
+
+
+
+
+//let textField = prompt("Enter text: ").split(" ");
+
+// let operand1 = parseInt(textField[0]);
+// let operand2 = parseInt(textField[2]);
+// let operator = textField[1];
+
+
+
+// operate(operand1, operand2, operator);
